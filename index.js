9aebf9901d34b760fee13166a74de5fab4d150e1
cofeekode <97682558+cofeekode@users.noreply.github.com>
2022-01-18 14:51:12 -0300

    Update index.js
    reativando BD e adicionando url no MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
-const url = "mongodb://localhost:27017";
-// const url = "mongodb+srv://admin:hd2rV5duoPrrIi3t@cluster0.jup2c.mongodb.net/";
+// const url = "mongodb://localhost:27017";
+const url = "mongodb+srv://admin:qbnSbQb2YA33-sz@cluster0.8t6ka.mongodb.net"
 
 const dbName = "ocean_bancodados_18_01_2022";
 
@@ -11,14 +11,12 @@ async function main() {
     
     async function main() {
     // Conexão com o Banco de Dados
-/*
+
     const client = await MongoClient.connect(url);
 
     const db = client.db(dbName);
 
     const collection = db.collection("herois");
-*/
-    const collection = undefined;
 
     // Aplicação em Express
 
